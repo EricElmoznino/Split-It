abd69da26f3d25cbfca7a32cb57f9dd0a8f09d95
ERIC ELMOZNINO <Eric@ERIC-iMac.local>
2015-07-08 16:52:19 -0400

    Initial Commit

---
+++ SplitIt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SplitIt
+//
+//  Created by ERIC ELMOZNINO on 2015-07-08.
+//  Copyright (c) 2015 Eric Elmoznino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
